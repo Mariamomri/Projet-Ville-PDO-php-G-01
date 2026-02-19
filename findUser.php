f821143cc4a06dd62e6436b7131238628cf7d154
Mariam <codegirlbxl@gmail.com>
2026-02-19 03:50:39 +0100

    created page read user/find user

--- findUser.php
+++ findUser.php
@@ -1,74 +1,71 @@
 <?php
-    $title = "Find User";
-    $nav = "findUser";
-    require "header.php";
-    if (!is_connected()){
-        header("location: ./login.php");
-    }
-    require "bd.php";
+$title = "Find User";
+$nav = "findUser";
+require "header.php";
+if (!is_connected()) {
+    header("location: ./login.php");
+}
+require "bd.php";
 ?>
-<center><b><h1>Find User</h1></b></center>
-<div align="center">
+<section class="yellow">
+    <center><b>
+            <h1>Find User</h1>
+        </b></center>
+    <div align="center">
         <div class="col-6">
-<?php
+            <?php
 
-    if (!empty($_POST['id'])):
-        try{
-            $resultat = $pdo->query("SELECT * FROM users WHERE id_user = ".$_POST['id']);
-            $user = $resultat->fetch(PDO::FETCH_OBJ);
-            if(!$user){  
-                echo "Utilisateur " . $_POST['id'].  " non trouvé";
-            } 
-        }catch (PDOException $e){
-            echo "Erreur : " . $e->getMessage();
-        }
-    endif;
-     
-    ?>
-    
+            if (!empty($_POST['id'])):
+                try {
+                    $resultat = $pdo->query("SELECT * FROM utilisateurs WHERE id_user = " . $_POST['id']);
+                    $user = $resultat->fetch(PDO::FETCH_OBJ);
+                    if (!$user) {
+                        echo "Utilisateur " . $_POST['id'] .  " non trouvé";
+                    }
+                } catch (PDOException $e) {
+                    echo "Erreur : " . $e->getMessage();
+                }
+            endif;
+
+            ?>
+
             <form action="./findUser.php" method="POST">
                 <input type="number" name="id" placeholder="id user" required>
                 <br>
-                
-                <button class="btn btn-primary" type="submit">Rechercher</button> 
+
+                <button class="btn-form-log" type="submit">Rechercher</button>
             </form>
 
-            <?php if(isset($user) && $user != false):?>
-        <form >
-            <br>
-            <label>Id : </label>
-            <input readonly value="<?php echo $user->id_user;?>">
-            <br>
-            <label>Prénom : </label>
-            <input readonly value="<?php echo $user->firstname;?>">
-            <br>
-            <label>Nom : </label>
-            <input readonly value="<?php echo $user->lastname;?>">
-            <br>
-            <label>Genre : </label>
-            <input readonly value="<?php echo $user->gender;?>">
-            <br>
-            <label>Date de Naissance : </label>
-            <input readonly value="<?php 
-            $datetime = new DateTime($user->date_of_birth);
-            $date = $datetime->format('d-m-Y');
-            echo $date;
-            ?>">
-            <br>
-            <label>Ville : </label>
-            <input readonly value="<?php echo $user->city;?>">
-            <br>
-            <label>Poids : </label>
-            <input readonly value="<?php echo $user->weight_kg . " Kg";?>">
-            <br>
-        </form>
-    
-    <?php endif;?>
+            <?php if (isset($user) && $user != false): ?>
+                <form>
+                    <br>
+                    <label>Id : </label>
+                    <input readonly value="<?php echo $user->id_user; ?>">
+                    <br>
+                    <label>Nom : </label>
+                    <input readonly value="<?php echo $user->nom; ?>">
+                    <br>
+                    <label>Prénom : </label>
+                    <input readonly value="<?php echo $user->prenom; ?>">
+                    <br>
+                    <label>Pseudo : </label>
+                    <input readonly value="<?php echo $user->pseudo; ?>">
+                    <br>
+                    <label>Mot de Passe : </label>
+                    <input readonly value="<?php echo "xxxxxx" ?>">
+                    <br>
+                    <label>Age: </label>
+                    <input readonly value="<?php echo $user->age; ?>">
+                    <br>
+                </form>
+
+            <?php endif; ?>
         </div>
-    </div>
+</section>
+</div>
 
 <?php
 
-    require "footer.php";
+require "footer.php";
 
 ?>
