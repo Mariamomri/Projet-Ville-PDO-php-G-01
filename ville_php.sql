fe9938ad3b68a46614295694db0730fede07b79d
Mariam <codegirlbxl@gmail.com>
2026-02-21 15:36:31 +0100

    update database

--- ville_php.sql
+++ ville_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 16, 2026 at 01:48 AM
+-- Generation Time: Feb 21, 2026 at 02:35 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -35,10 +35,61 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `pseudo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `mot_de_passe` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `age` int NOT NULL,
-  `id_ville` int DEFAULT NULL,
+  `id_user_ville` int DEFAULT NULL,
   PRIMARY KEY (`id_user`),
-  UNIQUE KEY `pseudo` (`pseudo`)
+  UNIQUE KEY `pseudo` (`pseudo`),
+  KEY `FK_User_ville` (`id_user_ville`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_user`, `nom`, `prenom`, `pseudo`, `mot_de_passe`, `age`, `id_user_ville`) VALUES
+(1, 'Omri', 'Mariam', 'Mary', '123', 30, 5),
+(2, 'Sahouli', 'Nisrine', 'art', '123', 25, 2),
+(5, 'Dunia', 'Julien', 'deoking', '123', 35, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `villes`
+--
+
+DROP TABLE IF EXISTS `villes`;
+CREATE TABLE IF NOT EXISTS `villes` (
+  `id_ville` int NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `pays` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `capitale` varchar(225) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_ville`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `villes`
+--
+
+INSERT INTO `villes` (`id_ville`, `nom`, `pays`, `capitale`) VALUES
+(1, 'Lion', 'France', 'Paris'),
+(2, 'Bruxelles', 'Belgique', 'Bruxelles'),
+(3, 'Berlin', 'Allemagne', 'Berlin'),
+(4, 'Granada', 'Espagne', 'Madrid'),
+(5, 'Bologna', 'Italie', 'Rome'),
+(6, 'Lisbonne', 'Portugal', 'Lisbonne'),
+(7, 'Amsterdam', 'Pays-Bas', 'Amsterdam'),
+(8, 'Vienne', 'Autriche', 'Vienne'),
+(9, 'Varsovie', 'Pologne', 'Varsovie'),
+(10, 'Athènes', 'Grèce', 'Athènes');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD CONSTRAINT `FK_User_ville` FOREIGN KEY (`id_user_ville`) REFERENCES `villes` (`id_ville`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
