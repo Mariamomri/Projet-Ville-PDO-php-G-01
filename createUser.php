e8718ed32fd4c23fabb45532a386b9b250cdf683
NisRin <sahoulinisrine@gmail.com>
2026-02-21 17:26:39 +0100

    correction page add Users

--- createUser.php
+++ createUser.php
@@ -15,64 +15,60 @@ require "bd.php";
     <div align="center">
         <div class="col-6">
             <?php
-
-            if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']) && !empty($_POST['mot_de_passe']) && !empty($_POST['age'])):
-
-
+            if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']) && !empty($_POST['mot_de_passe']) && !empty($_POST['age']) && !empty($_POST['ville'])):
                 $nom = $_POST['nom'];
                 $prenom = $_POST['prenom'];
                 $pseudo = $_POST['pseudo'];
                 $mot_de_passe = $_POST['mot_de_passe'];
                 $age = $_POST['age'];
+                $id_ville = $_POST['ville'];
 
                 try {
-                    $req = $pdo->prepare('INSERT INTO utilisateurs VALUES(:id_user, :nom, :prenom, :pseudo, :mot_de_passe, :age)');
-                    $req->execute(array(
-                        'id_user' => NULL,
+                    $req = $pdo->prepare('INSERT INTO utilisateurs (nom, prenom, pseudo, mot_de_passe, age, id_user_ville) 
+                                      VALUES(:nom, :prenom, :pseudo, :mot_de_passe, :age, :id_user_ville)');
+                    $req->execute([
                         'nom' => $nom,
                         'prenom' => $prenom,
                         'pseudo' => $pseudo,
                         'mot_de_passe' => $mot_de_passe,
                         'age' => $age,
-                    ));
-                    echo "L'utilisateur " . $nom . " " . $prenom . " a été ajouté<br>";
+                        'id_user_ville' => $id_ville
+                    ]);
+                    echo "L'utilisateur " . $nom . " " . $prenom . " a été ajouté !";
                 } catch (PDOException $e) {
                     echo "Erreur : " . $e->getMessage();
                 }
-
             else:
-                echo "Veuillez remplir les champs correctement";
+                if (!empty($_POST)):
+                    echo "Veuillez remplir les champs correctement";
+                endif;
             endif;
 
-
+            // Récupérer les villes
+            $villes = $pdo->query("SELECT * FROM villes ORDER BY nom")->fetchAll(PDO::FETCH_OBJ);
             ?>
 
             <form action="./createUser.php" method="POST">
-                <input type="text" name="nom" placeholder="Nom" required>
-                <br>
-                <input type="text" name="prenom" placeholder="Prénom" required>
-                <br>
-                <input type="text" name="pseudo" placeholder="Pseudo" required>
-                <br>
-                <input type="password" name="mot_de_passe" placeholder="Mot de passe" required>
-                <br>
-                <input type="number" name="age" placeholder="Age" required>
+                <input type="text" name="nom" placeholder="Nom" required><br>
+                <input type="text" name="prenom" placeholder="Prénom" required><br>
+                <input type="text" name="pseudo" placeholder="Pseudo" required><br>
+                <input type="password" name="mot_de_passe" placeholder="Mot de passe" required><br>
+                <input type="number" name="age" placeholder="Age" required><br><br>
+
+                <label>Ville :</label>
+                <select name="ville" required>
+                    <option value="">-- Sélectionnez une ville --</option>
+                    <?php foreach ($villes as $ville): ?>
+                        <option value="<?= $ville->id_ville ?>"><?= $ville->nom ?> (<?= $ville->pays ?>)</option>
+                    <?php endforeach; ?>
+                </select>
                 <br><br>
-
-
-
-                <!-- <select name="ville" required>
-                    <option value="Bruxelles">Bruxelles</option>
-                    <option value="Bologna">Bologna</option>
-                    <option value="Charlroi">Charlroi</option>
-                </select> -->
-                <br>
 
                 <button class="btn-form-log" type="submit">Ajouter</button>
             </form>
-            <br>
         </div>
     </div>
+
 </section>
 <?php
 
