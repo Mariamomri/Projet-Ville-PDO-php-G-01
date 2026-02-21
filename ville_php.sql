e87263f0f1f922a30007c8e63183b41fa2fc52ce
NisRin <sahoulinisrine@gmail.com>
2026-02-21 15:45:42 +0100

    database

--- ville_php.sql
+++ ville_php.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 16, 2026 at 01:48 AM
--- Server version: 9.1.0
--- PHP Version: 8.3.14
+-- Hôte : localhost:8889
+-- Généré le : sam. 21 fév. 2026 à 14:44
+-- Version du serveur : 8.0.40
+-- Version de PHP : 8.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,27 +18,104 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ville_php`
+-- Base de données : `ville_php`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateurs`
+-- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateurs` (
+  `id_user` int NOT NULL,
   `nom` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `prenom` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `pseudo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `mot_de_passe` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `age` int NOT NULL,
-  `id_ville` int DEFAULT NULL,
-  PRIMARY KEY (`id_user`),
-  UNIQUE KEY `pseudo` (`pseudo`)
+  `id_ville` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id_user`, `nom`, `prenom`, `pseudo`, `mot_de_passe`, `age`, `id_ville`) VALUES
+(1, 'Sahouli', 'Nisrine', 'niss', '123', 28, 2),
+(2, 'Omri', 'Mariam', 'mary', '123', 32, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `villes`
+--
+
+CREATE TABLE `villes` (
+  `id_ville` int NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `pays` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `villes`
+--
+
+INSERT INTO `villes` (`id_ville`, `nom`, `pays`) VALUES
+(1, 'Paris', 'France'),
+(2, 'Bruxelles', 'Belgique'),
+(3, 'Berlin', 'Allemagne'),
+(4, 'Madrid', 'Espagne'),
+(5, 'Rome', 'Italie'),
+(6, 'Lisbonne', 'Portugal'),
+(7, 'Amsterdam', 'Pays-Bas'),
+(8, 'Vienne', 'Autriche'),
+(9, 'Varsovie', 'Pologne'),
+(10, 'Athènes', 'Grèce');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`),
+  ADD KEY `id_ville` (`id_ville`);
+
+--
+-- Index pour la table `villes`
+--
+ALTER TABLE `villes`
+  ADD PRIMARY KEY (`id_ville`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `villes`
+--
+ALTER TABLE `villes`
+  MODIFY `id_ville` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD CONSTRAINT `utilisateurs_ibfk_1` FOREIGN KEY (`id_ville`) REFERENCES `villes` (`id_ville`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
